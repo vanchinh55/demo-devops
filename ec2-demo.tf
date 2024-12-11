4d45367e0ba6c39e1939893c32e81d612fa1a0df
vanchinh55 <huuchinh98bg1@gmail.com>
2024-12-12 02:38:32 +0700

    delete3306

--- ec2-demo.tf
+++ ec2-demo.tf
@@ -41,13 +41,6 @@ resource "aws_security_group" "chinh-security-group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
